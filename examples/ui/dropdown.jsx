1a3ef0997ceb1c389ec3dcb920459956106044ba
frederickk <ken.frederick@gmx.de>
2013-11-06 22:44:06 +0100

    added examples/ui/dropdown.jsx

---
+++ examples/ui/dropdown.jsx
@@ -0,0 +1,53 @@
+#targetengine 'basiljs'
+
+#includepath '~/Documents/;%USERPROFILE%Documents';
+#include 'basiljs/bundle/basil.js';
+
+
+var dialog;
+
+// var uiConfig = {
+//   theDropdown: {
+//     type: 'dropdown',
+//     label: 'Dropdown',
+//     items: ['item 1', 'item 2', 'item 3'],
+//     value: 'item 3',
+//     onChange: function(value) {
+//       b.println( 'My Dropdown has changed ' + value );
+//     }
+//   }
+// };
+
+
+
+function setup() {
+
+  // dialog = new b.ui.palette(
+  //   'Dropdown Example',
+  //   uiConfig
+  // );
+
+  dialog = new b.ui.dialog( b.PALETTE, 'Dropdown Example' );
+  dialog.add('dropdown', 'theDropdown', 'item 3', {
+    label: 'Dropdown',
+    items: ['item 1', 'item 2', 'item 3'],
+    onChange: function(value) {
+      b.println( 'My Dropdown has changed ' + value );
+    }
+  });
+
+};
+
+
+
+function draw() {
+};
+
+
+
+function update() {
+};
+
+
+
+b.go();
