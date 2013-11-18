d44c3bec0f4025f55ca7942dd278774723ca7dbe
frederickk <ken.frederick@gmx.de>
2013-11-18 23:12:05 +0100

    added 'Lines' options to example/ui/prompt_windows/phyllotactic_spiral.jsx

--- examples/ui/prompt_windows/phyllotactic_spiral.jsx
+++ examples/ui/prompt_windows/phyllotactic_spiral.jsx
@@ -29,27 +29,30 @@ function setup() {
 
   dialog = b.ui.palette( 'Phyllotactic Spiral' );
 
-  dialog.add('dropdown', 'drawModus', 'Circles', {
-    label: 'Dropdown',
-    items: ['Circles', 'Lines']
-  });
-
-  dialog.add('separator');
-
-  dialog.add('slider', 'amount', 20, {
-    label: 'Dot Count',
-    valueLabel: true,
-    valueType: 'int',
-    range: [10, 100]
-  });
   dialog.add('slider', 'spacing', 100, {
-    label: 'Dot Spacing',
+    label: 'Dot spacing',
     valueLabel: true,
     valueType: 'int',
     range: [10, 250]
   });
   dialog.add('textfield', 'radius', -1, {
     label: 'Dot Radius (-1 == auto)',
+  });
+
+  dialog.add('separator');
+
+  dialog.add('dropdown', 'drawModus', 'Circles', {
+    label: 'Drawing Mode',
+    items: ['Circles', 'Lines']
+  });
+
+  dialog.add('separator');
+
+  dialog.add('slider', 'amount', 20, {
+    label: 'Number of points',
+    valueLabel: true,
+    valueType: 'int',
+    range: [10, 100]
   });
 
   dialog.add('separator');
@@ -67,8 +70,14 @@ function setup() {
 
 function draw() {
 
-  b.noStroke();
-  b.fill( 'Black' );
+  if( dialog.drawModus === 'Circles' ) {
+    b.noStroke();
+    b.fill( 'Black' );
+  }
+  else {
+    b.noFill();
+    b.stroke( 'Black' );
+  }
 
   // draw some spirals
   var spiral = new Phyllotactic(
@@ -76,13 +85,21 @@ function draw() {
     b.height/2,
     dialog.amount,
     dialog.spacing,
-    dialog.radius
+    dialog.radius,
+    dialog.drawModus
   );
 
   if( dialog.bRandomColors ) {
     // fill with random color from swatch palette
     b.items(spiral, function(item, i) {
-      item.fillColor = getRandomSwatch();
+      if( dialog.bRandomColors ) {
+        if( dialog.drawModus === 'Circles' ) {
+          item.fillColor = getRandomSwatch();
+        }
+        else {
+          item.strokeColor = getRandomSwatch();
+        }
+      }
     });
   }
 
@@ -112,10 +129,11 @@ function update() {
  *  @param {Number} amount   number of spiral dots
  *  @param {Number} spacing  spacing between dots
  *  @param {Number} radius   radius of spiral's individual dots
+ *  @param {String} mode     'Circles' or 'Lines'
  *
  *  @return {Group} Group of Phyllotactic dots
  */
-var Phyllotactic = function(cx, cy, amount, spacing, radius) {
+var Phyllotactic = function(cx, cy, amount, spacing, radius, mode) {
   var amount = (amount != undefined)
       ? amount
       : 250,
@@ -141,7 +159,64 @@ var Phyllotactic = function(cx, cy, amount, spacing, radius) {
       radius = b.map(i, 0,amount, r*0.5,1);
     }
 
-    var dot = b.ellipse( x,y, radius,radius );
+    var dot;
+    if( mode === 'Lines' ) {
+      b.strokeWeight( radius );
+      dot = b.line(
+        x,y,
+        cx + (spacing * Math.sqrt(i-1) * Math.cos( (i-1) * b.radians( rotation ) )),
+        cy + (spacing * Math.sqrt(i-1) * Math.sin( (i-1) * b.radians( rotation ) ))
+      );
+
+      // TODO: add stroke options to core
+      // http://forums.adobe.com/thread/470458
+      // http://jongware.mit.edu/idcs5/pc_ObjectStyle.html
+      //
+      /* END CAPS */
+      // dot.endCap = EndCap.BUTT_END_CAP;
+      dot.endCap = EndCap.ROUND_END_CAP;
+      // dot.endCap = EndCap.PROJECTING_END_CAP;
+      //
+      /* END JOINS */
+      // dot.endJoin = EndJoin.MITER_END_JOIN;
+      dot.endJoin = EndJoin.ROUND_END_JOIN;
+      // dot.endJoin = EndJoin.BEVEL_END_JOIN;
+      //
+      /* ARROW HEADS */
+      // dot.leftLineEnd = ArrowHead.NONE;
+      // dot.leftLineEnd = ArrowHead.SIMPLE_ARROW_HEAD;
+      // dot.leftLineEnd = ArrowHead.SIMPLE_WIDE_ARROW_HEAD;
+      // dot.leftLineEnd = ArrowHead.TRIANGLE_ARROW_HEAD;
+      // dot.leftLineEnd = ArrowHead.TRIANGLE_WIDE_ARROW_HEAD;
+      // dot.leftLineEnd = ArrowHead.BARBED_ARROW_HEAD;
+      // dot.leftLineEnd = ArrowHead.CURVED_ARROW_HEAD;
+      // dot.leftLineEnd = ArrowHead.CIRCLE_ARROW_HEAD;
+      // dot.leftLineEnd = ArrowHead.CIRCLE_SOLID_ARROW_HEAD;
+      // dot.leftLineEnd = ArrowHead.SQUARE_ARROW_HEAD;
+      // dot.leftLineEnd = ArrowHead.SQUARE_SOLID_ARROW_HEAD;
+      // dot.leftLineEnd = ArrowHead.BAR_ARROW_HEAD;
+      //
+      // dot.rightLineEnd = ArrowHead.NONE;
+      // dot.rightLineEnd = ArrowHead.SIMPLE_ARROW_HEAD;
+      // dot.rightLineEnd = ArrowHead.SIMPLE_WIDE_ARROW_HEAD;
+      // dot.rightLineEnd = ArrowHead.TRIANGLE_ARROW_HEAD;
+      // dot.rightLineEnd = ArrowHead.TRIANGLE_WIDE_ARROW_HEAD;
+      // dot.rightLineEnd = ArrowHead.BARBED_ARROW_HEAD;
+      // dot.rightLineEnd = ArrowHead.CURVED_ARROW_HEAD;
+      // dot.rightLineEnd = ArrowHead.CIRCLE_ARROW_HEAD;
+      // dot.rightLineEnd = ArrowHead.CIRCLE_SOLID_ARROW_HEAD;
+      // dot.rightLineEnd = ArrowHead.SQUARE_ARROW_HEAD;
+      // dot.rightLineEnd = ArrowHead.SQUARE_SOLID_ARROW_HEAD;
+      // dot.rightLineEnd = ArrowHead.BAR_ARROW_HEAD;
+      //
+      /* ALIGNMENT */
+      // dot.strokeAlignment = StrokeAlignment.CENTER_ALIGNMENT
+      // dot.strokeAlignment = StrokeAlignment.INSIDE_ALIGNMENT
+      // dot.strokeAlignment = StrokeAlignment.OUTSIDE_ALIGNMENT
+    }
+    else {
+      dot = b.ellipse( x,y, radius,radius );
+    }
     dots.push( dot );
   }
 
